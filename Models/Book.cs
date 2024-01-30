02e946114eb32243917984f85f1e56e0db06c42e
Hakan Süpürkeci <45372929+hknsupurkeci@users.noreply.github.com>
2024-01-30 07:19:50 +0300

    Update Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -21,7 +21,7 @@ namespace LibraryApplication.Models
         public DateTime updated_at { get; set; }
 
         // BorrowedBook kaydına sahip olabileceğini gösterir. Yani, bir kitap (Book) birden fazla kez ödünç alınabilir ve her ödünç alınma olayı BorrowedBook sınıfıyla kaydedilir.
-        // Lazy load
+        // "Lazy Loading", ilişkili verilerin (bu durumda BorrowedBooks) sadece gerektiğinde yüklenmesini sağlar.
         public virtual ICollection<BorrowedBook>? BorrowedBooks { get; set; }
     }
 }
